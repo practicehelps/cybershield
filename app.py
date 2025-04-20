829a5703d1892fed44fa03248171809857b6634b
practicehelps <167209707+practicehelps@users.noreply.github.com>
2025-04-20 12:40:52 -0700

    Update app.py
    Accept image with IP addresses

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import pytesseract
 
 st.title("CyberShield - Coming Soon!")
+
+input_file_present = st.selectbox('PDF Input file?', ('No', 'Yes'))
+st.write('You selected:', input_file_present)
+
+input_txt = ""
+if input_file_present == "Yes":
+    image_file = st.file_uploader('Upload the image')
+
+    # wait until resume is uploaded
+    while image_file is None:
+        time.sleep(1)
+
+    text = pytesseract.image_to_string(image_file)
+    st.write("\nconverted text = %s\n" % text)
+
