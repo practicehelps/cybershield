3cfea3bdb2c16d92a44151e884339ff223c67601
practicehelps <167209707+practicehelps@users.noreply.github.com>
2025-04-20 16:54:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,8 @@ if input_file_present == "Yes":
         "Pick any IP address from the image and detect if it is malicious",
     )
     # Pass the ip addresses list as the context to the input_prompt
-    input_prompt += "\n%s" % text
     if st.button("Submit"):
         st.write("Logic to detect the maliciousness of the IP is in progress. Please check back in a while. Thank you.")
-        response = thought_action_pause_observation_loop(max_iterations=10, query=input_prompt)
+        response = thought_action_pause_observation_loop(max_iterations=10, query=input_prompt, context=text)
         st.write("response from initial experimental run: %s" % response)
 
